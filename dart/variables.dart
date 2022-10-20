4667779b679fb052e73f6c1ee8d3f804d9724b7d
Aysenur Perker <nurperker15@gmail.com>
2022-10-20 20:50:58 +0300

    Variables addded

---
+++ dart/variables.dart
@@ -0,0 +1,10 @@
+void main() {
+  var name = 'aysenur'; //String
+  var age = 23; // int
+  var hobby = ['Reading', 'Climbing']; //List(array)
+  var adress = {
+    'adress_1': 'Samsun',
+    'adress_2': 'Earth',
+  }; // Maps
+  print("$name,$age,$adress,$hobby");
+}
