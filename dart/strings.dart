a59e4bb834d5c7086a6fa93bbad40aff3233aa59
İbrahim Aydan <104987335+ibrahimaydan@users.noreply.github.com>
2022-10-20 21:05:20 +0300

    Strings commit

---
+++ dart/strings.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  String s0 = " ";
+  String s1 = "Gürkan";
+  String s2 = 'Fikret';
+  String s3 = 'Antalya\'da';
+  String s4 = "İstanbul'da \n İzmir'de";
+  String s5 = ''' 
+  
+  1 
+  2
+  3
+  4
+
+  ''';
+  print('$s0 $s1 $s2 $s3, $s4 $s5');
+}
