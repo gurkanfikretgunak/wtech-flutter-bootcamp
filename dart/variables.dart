0cc4539b0af884d3d134a627e58662ef2ea4363c
Ecem Hacıoğlu <74303639+ecemhacioglu@users.noreply.github.com>
2022-10-20 23:05:24 +0300

    update variables

---
+++ dart/variables.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  var name = 'Ecem';
+  var age = 24;
+
+
+  var hobby = ['movies', 'reading']; // List
+
+  var adress = {
+    // Map
+    "adress_1": "Bursa",
+    "adress_2": "Earth",
+  };
+
+  print('$name $age $hobby $adress');
+}
