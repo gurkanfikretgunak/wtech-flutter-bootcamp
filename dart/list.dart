907c11bdc83eff336995058d1a82d066150731e6
Gökalp Akoğlu <181805035@stu.adu.edu.tr>
2022-10-20 21:31:38 +0300

    list objects added

---
+++ dart/list.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  var l1 = [
+    1,
+    2,
+    3,
+    "",
+    [],
+    {"key": "value"},
+    {"value"}
+  ];
+  List l2 = <String>["Gökalp", "Melih", "Mete"];
+  List l3 = <int>[1, 2, 3, 4, 5];
+  List l4 = [1, "String", 1.2, []];
+
+  print("$l1,$l2,$l3,$l4");
+}
