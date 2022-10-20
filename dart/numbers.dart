af147647eaaadb708333b978c40e7cae4adc1631
Melih Çelik <melihcelik0909@gmail.com>
2022-10-20 21:22:16 +0300

    Numbers dart file created

---
+++ dart/numbers.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  int i1 = 1;
+  int i2 = -2;
+  int i3 = 0;
+  int i4 = 0xDEADBEEF;
+  double d1 = 1.0;
+  double d2 = 1.35;
+  double d3 = 1.44e5;
+  print('$i1 $i2 $i3 $i4 $d1 $d2 $d3');
+}
