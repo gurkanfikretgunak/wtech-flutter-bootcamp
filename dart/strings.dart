9d4e45cdcea9447622bbb1d20ed62cfd525141c8
Ulaş Öztürk <ulasozturkdev@gmail.com>
2022-10-20 21:04:59 +0300

    String Types

---
+++ dart/strings.dart
@@ -0,0 +1,25 @@
+void main(List<String> args) {
+
+
+  String s0 ="";
+  String s1 = 'ulas';
+  String s2 = 'ulass \n';
+  String s4 = 'antalya \'da \n';
+  String s5 = "İstanbul'da";
+  String s6 = '''    
+    a
+    b
+    c
+    d
+    e
+    f
+    g
+    h
+  
+  
+  ''';// buraya gelen her enter \n olmadan alt satıra gecer
+  
+
+  print("$s0   $s1   $s2    $s4    $s5   ");
+  print(s6);
+}
