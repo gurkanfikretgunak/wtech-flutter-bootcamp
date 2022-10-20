cbaf53dd22bc70094fe960dcaa0c7cde099c8d67
ArdaNutyali <64331573+ArdaNutyali@users.noreply.github.com>
2022-10-20 21:04:57 +0300

    string objects

---
+++ dart/strings.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  //String object
+  String s0 = " ";
+  String s1 = "Arda";//Double Quote
+  String s2 = 'Arda';//Single Quote
+  String s3 = 'Istanbul \'da';
+  String s4 = "Istanbul 'da";
+  String s5 = '''
+
+  1
+  2
+  3
+  4 
+  
+  ''';
+
+  print('$s0, $s1, $s2, $s3, $s4, $s5');
+}
