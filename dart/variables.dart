641f7067a147743964b895a20da3b26788a38ceb
İbrahim Aydan <104987335+ibrahimaydan@users.noreply.github.com>
2022-10-20 20:46:23 +0300

    Variables added and printed

---
+++ dart/variables.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  var name = 'gurkan'; //string
+  var age = 31; //int
+  var weight = 98.5; // double
+  var hobby = [
+    "Movies",
+    "Running",
+  ]; //list (array)
+  var adress = {"adress_1": "Istanbul", "adress_2": "Earth"};
+
+  print("$name,$age,$weight,$hobby,$adress");
+}
