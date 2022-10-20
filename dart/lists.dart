e595cb88c5b258209cfb059da09f94145867d0e9
Melih Çelik <melihcelik0909@gmail.com>
2022-10-20 21:34:27 +0300

    Lists dart file created.

---
+++ dart/lists.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  var l1 = [1, 2, 3];
+  List<int> l2 = [1, 2, 3, 4, 5];
+  List<String> l3 = ['Melih', 'Mete', 'Gökalp'];
+  List l4 = [1, 'Melih', 1.2, true, []];
+  print('$l1 $l2 $l3 $l4');
+  print(l3[0]);
+}
