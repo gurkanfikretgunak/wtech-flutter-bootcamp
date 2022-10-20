4966638cf5ac1e7ff6e6d7ea4b0c2e2cf45cbd50
Gökalp Akoğlu <181805035@stu.adu.edu.tr>
2022-10-20 21:04:35 +0300

    string objects

---
+++ dart/strings.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  String s0 = " ";
+  String s1 = "Gökalp";
+  String s2 = 'Akoğlu';
+  String s3 = 'Aydın\'da';
+  String s4 = "İzmir'de";
+  String s5 = '''
+
+  1
+  2
+  3
+  4
+  ''';
+
+  print('$s0,$s1,$s2,$s3,$s4,$s5');
+}
