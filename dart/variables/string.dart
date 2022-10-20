96362d07373e8ea598e0c0eb6306ef625d6c86a0
Aysenur Perker <nurperker15@gmail.com>
2022-10-20 21:10:35 +0300

    String instances added

---
+++ dart/variables/string.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  String s0 = "";
+  String s1 = "Aysenur";
+  String s2 = 'Perker';
+  String s3 = 'Antalya \ da';
+  String s4 = "Istanbul'da Izmirde'de ";
+  String s5 = '''
+
+1
+2
+3
+4
+''';
+  //String = List(Array Object)
+  String s6 = 'Aysenur' 'Perker';
+  String s7 = r'Raw String';
+
+  print("$s0,$s1,$s2,$s3,$s4,$s5, $s6,$s7");
+}
