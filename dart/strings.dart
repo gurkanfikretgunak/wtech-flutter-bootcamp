59d9a43960ce93d388d81e71413a6aac6e186e30
ONUR ŞAHİN <1803022060@student.beykent.edu.tr>
2022-10-20 21:04:53 +0300

    String Object

---
+++ dart/strings.dart
@@ -0,0 +1,18 @@
+void main() {
+  //String is a object.
+
+  String s0 = " ";
+  String s1 = "Onur";
+  String s2 = 'Sahin';
+  String s3 = 'Istanbul \'da';
+  String s4 = "Sivas'ta";
+  String s5 = '''
+
+  1
+  2
+  3
+  4
+  ''';
+
+  print('$s0,$s1,$s2,$s3,$s4,$s5');
+}
