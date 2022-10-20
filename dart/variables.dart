559f660503687f48b1a67d9e2ff13e7fdae1505e
ArdaNutyali <64331573+ArdaNutyali@users.noreply.github.com>
2022-10-20 20:48:34 +0300

    Method and Variables Added!

---
+++ dart/variables.dart
@@ -0,0 +1,10 @@
+void main() {
+  var name = "arda";//string
+  var age = 21;//int
+  var weight = 80;//double
+  var hobby = ["Movies", "Running", ""];//list(array)
+
+  var address = {"address_1": "Istanbul", "address_2": "Earth"};
+
+  print("$name, $age, $weight, $hobby, $address");
+}
