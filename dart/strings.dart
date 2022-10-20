012d6bf55c1245c25e6e1ee398fe51bc7ab94cb9
cantanrikulu <73619391+cantanrikulu@users.noreply.github.com>
2022-10-20 21:14:01 +0300

    added variables

---
+++ dart/strings.dart
@@ -0,0 +1,21 @@
+void main(List<String> args) {
+  //string object
+
+  String s0 = " ";
+  String s1 = "Can";
+  String s2 = 'Tanrıkulu';
+  String s3 = 'Bursa\ da';
+  String s4 = " İstanbul'da ";
+  String s5 = ''' 
+  
+  1
+  2
+  3
+  4
+
+  ''';
+
+  String s6 = 'Ahmet' 'Can' 'Tanrıkulu';
+
+  //String=List(array)
+}
---
+++ dart/variables.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  var name = 'can';
+  var age = '23';
+  var weight = '175.5';
+  var hobby = ['footbal', 'game'];
+
+  print('$name,$age,$weight,$hobby');
+}
