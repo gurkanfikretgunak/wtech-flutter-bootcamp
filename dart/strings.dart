b36f2d24df91fcc870c844cc3bb7c0ca8777a3bf
Aslınaz Dayı <aslinazdayi@gmail.com>
2022-10-20 21:07:13 +0300

    String objects multi quote added

---
+++ dart/strings.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  String s0 =" ";
+  String s1 = "Aslınaz";
+  String s2= 'Dayı';
+  String s3 = 'Antalya\'da';
+  String s4 = "Istanbul'da";
+  String s5='''
+  
+  1
+  2
+  3
+  4
+  
+  ''';
+
+  String s6= 'Aslınaz' 'Dayı' 'İstanbul';
+
+  print('$s0,$s1,$s2,$s3,$s4,$s5,$s6');
+}
