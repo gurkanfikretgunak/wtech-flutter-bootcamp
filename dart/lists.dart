0c87c47b0d7339e55ce650b7f3792eef30f9417d
bhrzybk <103609875+bhrzybk@users.noreply.github.com>
2022-10-20 23:51:16 +0300

    lists added

---
+++ dart/lists.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  var l1 = [
+    1,
+    2,
+    3,
+    4,
+    'sl',
+    1,
+    2,
+    {"key:value"},
+    {"value"}
+  ];
+
+  List l2 = <int>[1, 2, 3, 4];
+  List l3 = <String>['Bahar', 'Merve'];
+
+  print('$l1 , $l2 , $l3');
+}
