e0ff58302303a5666a6cc6b314464803087cd6a3
Batuhan-Akdemirr <85792583+Batuhan-Akdemirr@users.noreply.github.com>
2022-10-20 21:30:41 +0300

    boolean and list object added

---
+++ dart/list.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  var l1 = [
+    1,
+    2,
+    3,
+    4,
+    'sl',
+    1,
+    2,
+    {"key:value"},
+    {"value"}
+  ];
+
+  List l2 = <int>[1, 2, 3, 4];
+  List l3 = <String>['Batuhan', 'Ali'];
+
+ 
+
+  print('$l1 , $l2 , $l3');
+}
