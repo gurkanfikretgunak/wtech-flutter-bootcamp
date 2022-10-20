28a21e440101713fdbb3a18c52204e5969a968c4
ONUR ŞAHİN <1803022060@student.beykent.edu.tr>
2022-10-20 20:46:34 +0300

    Variables added, tested, printed.

---
+++ dart/variables.dart
@@ -0,0 +1,12 @@
+void main() {
+  var name = "Onur"; //string
+  var age = 23; //integer
+  var weight = 52.8; //double
+  var hobby = ["Movies", "Computer"]; //list (array)
+  var adress = {
+    "adress_1": "Istanbul",
+    "adress_2": "Sivas",
+  }; //Maps
+
+  print("$name, $age, $weight,$hobby,$adress");
+}
