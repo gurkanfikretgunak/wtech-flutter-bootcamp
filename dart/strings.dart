839a1ea6c53ca7412f791afa58e09a82c2ad64c1
Melih Çelik <melihcelik0909@gmail.com>
2022-10-20 21:05:24 +0300

    Strings dart file created.

---
+++ dart/strings.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  //String is a object.
+  String s0 = "";
+  String s1 = "Hello";
+  String s2 = 'Hello';
+  String s3 = 'Melih\'s World';
+  String s4 = "Melih's World";
+  String s5 = '''
+
+1
+2
+3
+4
+
+''';
+  print('$s0 $s1 $s2 $s3 $s4 $s5');
+}
