4a59021fb7a17936e6a57b7b24499a367f00f346
bhrzybk <103609875+bhrzybk@users.noreply.github.com>
2022-10-20 23:39:39 +0300

    string object

---
+++ dart/strings.dart
@@ -0,0 +1,21 @@
+void main(List<String> args) {
+  String s0 = " ";
+  String s1 = "Bahar";
+  String s2 = 'Zeybekoğlu';
+  String s3 = 'Siirt';
+  String s4 = " İstanbul'da ";
+  String s5 = ''' 
+  
+  1
+  2
+  3
+  4
+  5
+  ''';
+
+  String s6 = 'Bahar' 'Zybk' 'istanbul';
+
+  print("$s0,$s1,$s2,$s3,$s4,$s5");
+
+  //String=List(array)
+}
