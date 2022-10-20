fcba87b23f8151f5d67aa6b3f66a47abdd5f204d
Batuhan-Akdemirr <85792583+Batuhan-Akdemirr@users.noreply.github.com>
2022-10-20 21:05:01 +0300

    string objects

---
+++ dart/strings.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  String s0 = " ";
+  String s1 = "Batuhan";
+  String s2 = 'Akdemir';
+  String s3 = 'Antalya\'da';
+  String s4 = "İstanbul'da\nİzmir'de";
+  String s5 = '''  
+  1
+  2
+  3
+  4
+   ''';
+
+  print('$s0 $s1 $s2 $s3 $s4 $s5');
+}
