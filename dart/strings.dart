58809e4798e1911b5ff48db03e75beaf95f0ef73
metecoban <64808380+metecoban@users.noreply.github.com>
2022-10-20 21:13:07 +0300

    Strings file added

---
+++ dart/strings.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  String s0 = ' '; //Blank cell takes up space
+  String s1 = "Mete";
+  String s2 = 'Coban';
+  String s3 = 'Amasya \'da';
+  String s4 = "Amasya'da";
+  String s5 = '''
+
+1
+2
+3
+4
+''';
+  // String = List(Array Object)
+  String s6 = 'Hello' 'Wtech' 'Family'; //Strings are also array objects
+  String s7 = r'Raw object';
+  print('$s1,$s2,$s3,$s4,$s5$s6,$s7');
+}
