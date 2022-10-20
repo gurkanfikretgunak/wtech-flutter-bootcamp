a31ac67bf06e908fa2057a5127ed262edf2281b2
emineealtuntas <111460539+emineealtuntas@users.noreply.github.com>
2022-10-20 21:22:30 +0300

    numbers added

---
+++ dart/numbers.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  int i1 = 1;
+  int i2 = -2;
+  int i3 = 0;
+  int i4 = 0xADEADBEEF;
+  double d1 = 1.0;
+  double d2 = 1.35;
+  double d3 = 1.14e5;
+
+  print("$i1,$i2,$i3,$i4,$d1,$d2,$d3");
+
+  // web type affetmez!
+}
