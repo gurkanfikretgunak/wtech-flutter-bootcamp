5312682b6fb43344f61aab764bc6a8271abd6944
metecoban <64808380+metecoban@users.noreply.github.com>
2022-10-20 21:23:48 +0300

    Numbers file added

---
+++ dart/numbers.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  int i1 = 1;
+  int i2 = -2;
+  int i3 = 0;
+  int i4 = 0xDEADBEEF; // We can also declare hex as int
+
+  double d1 = 1.0; // Web type important
+  double d2 = 1.35;
+  double d3 = 1.42e5;
+
+  // Convert type are using -like intVariable.toString();
+
+  print('$i1,$i2,$i3,$i4,$d1,$d2,$d3');
+}
