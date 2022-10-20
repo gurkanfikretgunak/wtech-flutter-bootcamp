5bb541b66c54c597e122fab342052827a91513c1
Beyza Aktürk <48009801+beyzakturk@users.noreply.github.com>
2022-10-20 20:59:14 +0300

    add variables

---
+++ dart/variables.dart
@@ -0,0 +1,10 @@
+void main() {
+  var name = 'Beyza'; //String
+  var age = 23; // int
+  var hobby = ['Reading', 'Gameing']; //List(array)
+  var adress = {
+    'adress_1': 'Amasya',
+    'adress_2': 'Trabzon',
+  }; // Maps
+  print("$name,$age,$adress,$hobby");
+}
