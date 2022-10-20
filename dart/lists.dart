35cdf16995195f4d1d29d45c67ffd835a24f557b
Aysenur Perker <nurperker15@gmail.com>
2022-10-20 21:30:56 +0300

    lists objects added

---
+++ dart/lists.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  var l1 = [
+    1,
+    2,
+    3,
+    "",
+    1.2,
+    {"value"}
+  ];
+
+  List l2 = [1, 2, 3, 4];
+  List l3 = <String>["a", "b", "c"];
+  List l4 = [];
+  print('$l4,$l2,$l3,$l1');
+}
