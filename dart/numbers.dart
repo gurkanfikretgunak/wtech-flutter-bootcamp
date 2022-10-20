e2cbb36d26491d4c7d3c35d9288088815358eec3
Beyza Aktürk <48009801+beyzakturk@users.noreply.github.com>
2022-10-20 21:26:55 +0300

    add numbers

---
+++ dart/numbers.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  int a1 = 1;
+  int a2 = -1;
+  int a3 = 0;
+  int a4 = 0xADBEEFDE;
+  double d1 = 2.0;
+  double d2 = 1.38;
+  double d3 = 1.44e5;
+
+  print("$a1 $a2 $a3 $a4 $d1 $d2 $d3");
+}
