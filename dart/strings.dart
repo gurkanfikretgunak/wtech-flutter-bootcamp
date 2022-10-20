3c18aa817123aa8136e32b993f035d0320814766
Gürkan Fikret Günak <52853374+gurkanfikretgunak@users.noreply.github.com>
2022-10-20 21:04:54 +0300

    string objects

---
+++ dart/strings.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  // String object
+
+  String s0 = " ";
+  String s1 = "Gurkan";
+  String s2 = 'Fikret';
+  String s3 = 'Antalya \'da';
+  String s4 = "Istanbul'da İzmir'de ";
+  String s5 = ''' 
+
+  1
+  2
+  3
+  4 
+  
+  ''';
+
+  print('$s0,$s1,$s2,$s3,$s4,$s5');
+}
