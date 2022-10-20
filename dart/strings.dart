d054dceee6a9d6ef394f0d58382ed4d8b69ea7f2
Ecem Hacıoğlu <74303639+ecemhacioglu@users.noreply.github.com>
2022-10-20 21:15:01 +0300

    string objects

---
+++ dart/strings.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  //String is a object.
+  String s0 = "";
+  String s1 = "Hello";
+  String s2 = 'Hi';
+  String s3 = 'Ecem\'s World';
+  String s4 = "Ecem's World";
+  String s5 = '''
+
+1
+2
+3
+4
+
+''';
+  print('$s0 $s1 $s2 $s3 $s4 $s5');
+}
