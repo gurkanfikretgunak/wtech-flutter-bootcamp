ae511e2be742ff4091070f9cd9747a5e7400fca9
ArdaNutyali <64331573+ArdaNutyali@users.noreply.github.com>
2022-10-20 21:22:02 +0300

    numbers objects added

---
+++ dart/numbers.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  int i1 = 1;
+  int i2 = -2;
+  int i3 = 0;
+  int i4 = 0xDEADBEEF;
+  double d1 = 1.0;//1.0 = 1
+  double d2 = 1.35;
+  double d3 = 1.42e5;
+  double d4 = 1;//1.0
+
+  print("$i1, $i2, $i3, $i4, $d1, $d2, $d3");
+}
