00854bd3150c906a6189afad8cf70fa3271bd364
Gökalp Akoğlu <181805035@stu.adu.edu.tr>
2022-10-20 20:50:23 +0300

    variables added

--- dart/variables.dart
+++ dart/variables.dart
@@ -1,9 +1,10 @@
 void main(List<String> args) {
   var name = 'Gökalp';
+  var surname = "Akoğlu";
   var age = 22;
   var weight = 89.6;
   var hobby = ['Movies', 'Football', 'Games'];
   var address = {'address_1': 'Aydın', 'address_2': 'İzmir'};
 
-  print("$name,$age,$weight,$hobby,$address");
+  print("$name,$surname,$age,$weight,$hobby,$address");
 }
