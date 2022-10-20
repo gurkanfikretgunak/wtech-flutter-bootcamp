989f37ac48231141617a44327a3521b884d2cf33
Beyza Aktürk <48009801+beyzakturk@users.noreply.github.com>
2022-10-20 21:15:29 +0300

    add strings

---
+++ dart/strings.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  String s0 = " ";
+  String s1 = "Beyza";
+  String s2 = 'Aktürk';
+  String s3 = 'Amasya\'da';
+  String s4 = "Trabzon'da";
+  String s5 = '''
+  
+  1
+  2
+  3
+  4
+  
+  ''';
+
+  String s6 = 'Beyza' 'Aktürk' 'Trabzon';
+
+  print('$s0 $s1 $s2 $s3 $s4 $s5 $s6');
+}
