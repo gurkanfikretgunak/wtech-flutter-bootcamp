a4bffc1f74d65bca0fc6c7578e837ea5aef40924
Melih Çelik <melihcelik0909@gmail.com>
2022-10-20 20:48:41 +0300

    Variables Added

---
+++ dart/variables.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  var name = 'Melih';
+  var age = 22;
+  var height = 1.87;
+  var hobbies = ['Coding', 'Watching Series', 'Playing Games'];
+  var address = {'street': 'Cumhuriyet Mahallesi', 'city': 'Aydın', 'country': 'Turkey'};
+  print('$name');
+}
