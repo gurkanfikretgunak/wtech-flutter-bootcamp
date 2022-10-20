b2338387062763b1c9b78b574d7a1a696ad0c37e
Aysenur Perker <nurperker15@gmail.com>
2022-10-20 21:22:21 +0300

    numbers objects added

---
+++ dart/numbers.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+// Web type affetmez.
+
+  int i1 = 1;
+  int i2 = -2;
+  int hex = 0xADBEEFDE;
+  double d1 = 1.0;
+  double d2 = 1.44e6;
+
+  print("$d1,$hex,$i2,$i1,$d2");
+}
