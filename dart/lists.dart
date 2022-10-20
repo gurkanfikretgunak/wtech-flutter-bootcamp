0408df6e2e53b4b4f12515875a8da4874636dc38
Beyza Aktürk <48009801+beyzakturk@users.noreply.github.com>
2022-10-20 21:33:58 +0300

    add lists

---
+++ dart/lists.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  var l1 = {
+    1,
+    2,
+    3,
+    ["values"],
+  };
+  List l2 = <int>[1, 2, 3, 4];
+  List l3 = <String>["Beyza", "Ayşe", "Tiana"];
+  print("$l1 $l2 $l3");
+}
