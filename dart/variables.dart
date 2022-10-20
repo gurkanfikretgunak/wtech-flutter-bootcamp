ae4a7949c9176c27e0fb0cf330899886c857c0aa
Batuhan-Akdemirr <85792583+Batuhan-Akdemirr@users.noreply.github.com>
2022-10-20 20:46:45 +0300

    Variables added and tested

---
+++ dart/variables.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  var name = 'Batuhan';
+  var age = 22;
+  var weight = 77.2;
+
+  var hobby = ['movies', 'running']; // List
+
+  var adress = {
+    // Map
+    "adress_1": "Istanbul",
+    "adress_2": "Earth",
+  };
+
+  print('$name $age $weight $hobby $adress');
+}
