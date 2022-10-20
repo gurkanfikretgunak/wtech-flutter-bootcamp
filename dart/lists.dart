05ce7d37a6cbbced18d4cffdf6280c438112e040
Ulaş Öztürk <ulasozturkdev@gmail.com>
2022-10-20 21:30:36 +0300

    list

---
+++ dart/lists.dart
@@ -0,0 +1,12 @@
+
+void main(List<String> args) {
+  
+  List<int> _intlist = [1,2,3,4,5,6,7,8];
+  List<double>  ulas = [];
+
+
+  List aaa = <int>[12,13,14,15,16];
+  List bbb = <String>["a","b","c","d"];
+
+  print("$aaa   $bbb");
+}
