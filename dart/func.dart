a27cea8a2c6c27ad1f3832f597509fce6ab072ca
Muhammed18-09 <79153020+Muhammed18-09@users.noreply.github.com>
2022-10-21 21:03:17 +0300

    Create func.dart

---
+++ dart/func.dart
@@ -0,0 +1,19 @@
+
+getemeil(){
+
+}
+
+getphone(String email, int password){
+}
+
+void getPassword(){}
+
+String named() => "String function";
+
+
+void main() {
+  print(getphone("muhammed", 1234567));
+  
+  
+}
+
