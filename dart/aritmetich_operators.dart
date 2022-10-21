02c7cfc38f0685bdb1007fe38b108a158d575816
Batuhan-Akdemirr <85792583+Batuhan-Akdemirr@users.noreply.github.com>
2022-10-21 22:50:35 +0300

    arithmetic operations added

---
+++ dart/aritmetich_operators.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  int number1 = 17, number2 = 32;
+
+  int sum = number1 + number2;
+  int subtract = number1 - number2;
+  int multi = number1 * number2;
+  int divide = number1 ~/ number2;
+  int mode = number2 % number1;
+
+  print('$sum , $subtract $multi , $divide , $mode ');
+}
