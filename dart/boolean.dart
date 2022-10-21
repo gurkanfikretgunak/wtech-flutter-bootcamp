b3582aab4da2c74f229e9abffcf48349311816b5
Simay4955 <112400160+Simay4955@users.noreply.github.com>
2022-10-21 15:19:06 +0300

    boolean  variables added

---
+++ dart/boolean.dart
@@ -0,0 +1,4 @@
+void main(List<String> args) {
+  bool b1 = true;
+  bool b2 = false;
+}
---
+++ dart/numbers.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  int i1 = 1;
+  int i2 = -5;
+  int i3 = 0;
+  int i4 = 0xADBEEFDE;
+  double d1 = 1.0;
+  double d2 = 1.38;
+  double d3 = 1.44e5;
+
+  print("$i1,$i2, $i3 , $i4 ,  $d1 , $d2 , $d3");
+}
