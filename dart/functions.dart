5feddfd4bc0494e146cb6697e3231dba0267d366
Ulaş Öztürk <ulasozturkdev@gmail.com>
2022-10-21 20:46:58 +0300

    functions added

---
+++ dart/functions.dart
@@ -0,0 +1,34 @@
+
+signInWithEmail(){
+  //TODO sign-in services
+
+}
+
+
+signInWithPhone(){
+  //TODO sign-in with phone services
+  
+}
+
+
+getEmail() {
+
+}
+emailSender(){
+
+}
+
+emailService(){
+  getEmail();
+  emailSender();
+  
+  //hepsini burda çağırırsın
+}
+
+String signInWithGithub() => "string dönmek zorundasın moruk";  //böyle de kullanılabilir
+
+
+
+void main(List<String> args) {
+  
+}
