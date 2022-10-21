d78aa968ce32567efd4dccef29cdfa28a00a74f6
Ahmet Aydın <84200491+ahmtydn@users.noreply.github.com>
2022-10-21 21:06:40 +0300

    Functions learned

---
+++ functions.dart
@@ -0,0 +1,51 @@
+
+
+
+  signInWithEmail(){
+   //TODO sign-in services E-mail sevices
+  }
+
+    signInWithPhone(){
+   //TODO sign-in services Phone sevices
+  }
+
+String signInWithGithub()=>"String return";
+
+void getGitHubUser(){
+    
+}
+
+void deleteUserfromDB(){
+  //return yapılan işlem
+}
+ //Kurgu örneği
+  getEmail(){
+
+  }
+
+  emailSender(){
+
+  }
+
+  emailServices(){
+    getEmail();
+    emailSender();
+  }
+
+//parametre
+  signInWithEmailAndPassword(String email,String password){
+
+    
+
+  }
+
+   signInWithEmailAndPasswordNew({required String? email,String? password}){
+
+    
+
+  }
+
+  void main(List<String> args) {
+  signInWithEmailAndPassword("ahmt@gmail.com", "1234");
+  signInWithEmailAndPasswordNew(email: "");
+}
