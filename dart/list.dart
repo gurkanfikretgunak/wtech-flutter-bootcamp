e160fabae23f00cda1517abaa22c68d4bba9ff11
Simay4955 <112400160+Simay4955@users.noreply.github.com>
2022-10-21 15:26:19 +0300

    list   map added

---
+++ dart/list.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  var l1 = [
+    1,
+    2,
+    3,
+    4,
+    "",
+    {"key": "value"}
+  ];
+
+  List l2 = <int>[1, 2, 3, 4, 5];
+
+  List l3 = <String>[" Rukiye ", "Simay ", "Asrak"];
+
+  print("$l1,$l2 ,$l3");
+}
