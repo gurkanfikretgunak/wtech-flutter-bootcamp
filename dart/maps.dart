a9da53455fe8533919fe674607280cb04d3338e4
Beyza Aktürk <48009801+beyzakturk@users.noreply.github.com>
2022-10-21 20:37:31 +0300

    add sets and maps

---
+++ dart/maps.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  var n1 = {"Key": "Value"};
+  var n2 = {1: "Value"};
+  //HashMap gibi map
+  var n3 = Map<String, dynamic>();
+  n3['Key1'] = "Value1";
+  n3['Key2'] = "Value2";
+  n3['Key3'] = "Value3";
+
+  print(n3);
+}
---
+++ dart/sets.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  var names = {"value", "values"};
+  Set s2 = {"value", "value2"};
+  Set s3 = <String>{"value", "value2"};
+  var e = <String>{};
+  e.add('deneme');
+  print(e);
+  // spread operator listleri birbirine bağlamak için kullanılır.
+}
