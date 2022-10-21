698d9bdaeb952cc591746418acc88c9706f47d9a
Gürkan Fikret Günak <52853374+gurkanfikretgunak@users.noreply.github.com>
2022-10-21 20:36:01 +0300

    lists,maps,sets added

--- dart/lists.dart
+++ dart/lists.dart
@@ -15,5 +15,7 @@ void main(List<String> args) {
   List l3 = <String>["Gurkan", "İbrahim", "Ali", "Çiğdem", "Aslı"];
   List l4 = [1, "String", 1.2, []];
 
+  List l5 = [1, 2, 3, ...l4];
+
   print('$l1,$l2,$l3,$l4');
 }
