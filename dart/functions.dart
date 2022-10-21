c4522c19cd7d9056dc3e2d11205561af6d67c31d
Batuhan-Akdemirr <85792583+Batuhan-Akdemirr@users.noreply.github.com>
2022-10-21 21:16:33 +0300

    functions added

---
+++ dart/functions.dart
@@ -0,0 +1,16 @@
+signWithEmail() {}
+
+signWithPhone() {}
+
+// Arrow
+String signWithGithub() => " ";
+
+void getGithubUser() {}
+
+void deleteFromDB() {}
+
+signWithEmailAndAdress(String batuhan, {String email = "", Stringpassword = ""}) {}
+
+void main(List<String> args) {
+  signWithEmailAndAdress('Batuhan', email: '@.com', Stringpassword: "1234");
+}
