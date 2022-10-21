ad4482a9e4562c553c849660c6ffb1e71a1c6e65
cantanrikulu <73619391+cantanrikulu@users.noreply.github.com>
2022-10-21 23:25:40 +0300

    funcitons added

---
+++ dart/functions.dart
@@ -0,0 +1,22 @@
+signInWithEmail() {
+//TODO sign-in with E-mail service
+}
+
+signInWithPhone() {
+  //TODO sign-in with phone service
+}
+
+String signInWithGithub() => "String function value returning";
+
+void getGithubUser() {}
+
+void deleteUserfromDB() {}
+
+signInWithEmailAndPassword({required String? email, String? password}) {}
+
+signInWithEmailAndPasswordNew(String mail, String password) {}
+
+void main(List<String> args) {
+  signInWithEmailAndPassword(email: "", password: "");
+  signInWithEmailAndPasswordNew("", "");
+}
