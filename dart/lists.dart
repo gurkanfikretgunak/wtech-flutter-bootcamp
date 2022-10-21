dbf5af0fecb36bf54786747cf61b93cc501dbde1
emineealtuntas <111460539+emineealtuntas@users.noreply.github.com>
2022-10-21 10:04:15 +0300

    string objects

---
+++ dart/lists.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  var 11 = [
+    1,
+    2,
+    3,
+    4,
+    "",
+    1.2,
+    [],
+    {"key" : "value"}
+    ];
+
+  List 12 = <int>[1, 2, 3, 4, 5];
+  List 13 = <String> ["Gürkan"];
+
+  print("$")
+}
---
+++ dart/strings.dart
@@ -0,0 +1,19 @@
+void main() {
+//String object
+
+  String s0 = " ";
+  String s1 = "Emine";
+  String s2 = 'Gürkan';
+  String s3 = 'Antalya\'da';
+  String s4 = "İstanbul'da";
+  String s5 = '''
+
+  1
+  2
+  3
+  4
+
+  ''';
+
+  print('$s0, $s1, $s2, $s3, $s4, $s5');
+}
