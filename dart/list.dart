acf8b4e8e5b502425046787888623fea65832a2f
Gökalp Akoğlu <181805035@stu.adu.edu.tr>
2022-10-21 20:37:24 +0300

    sets and maps objects added

--- dart/list.dart
+++ dart/list.dart
@@ -12,5 +12,7 @@ void main(List<String> args) {
   List l3 = <int>[1, 2, 3, 4, 5];
   List l4 = [1, "String", 1.2, []];
 
-  print("$l1,$l2,$l3,$l4");
+  List l5 = [1,2,3,...l4];//listeleri birbirine bağlama
+
+  print("$l1,$l2,$l3,$l4,$l5");
 }
---
+++ dart/maps.dart
@@ -0,0 +1,26 @@
+void main(List<String> args) {
+  var m1 = {"Key": "Value"};
+  var m2 = {1: "Value"};
+  var m3 = {[], "Value"};
+
+  var m4 = Map<String, dynamic>();
+
+  m4["Key1"] = "Value1";
+  m4["Key2"] = "Value2";
+  m4["Key3"] = "Value3";
+
+  Map<String, dynamic> m5 = {
+    "sID": 9426225,
+    "access-token": "12432443asddfgsdfda.sdgfsasd",
+    "username": "gokalpakoglu",
+    "emailVerify": true,
+    "oldUserData": false,
+    "last-access-point": [132, 24, 59],
+    "documents": {
+      "docID": 12321421,
+      "bucketLink": "http://file.sample.com/userId/12321421",
+    }
+  };
+
+  print('$m1,$m2,$m3,$m4,$m5');
+}
