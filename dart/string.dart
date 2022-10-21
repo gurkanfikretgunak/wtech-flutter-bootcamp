1113ebd2d39b2edf7e6b8aa236db176edb2430fd
Simay4955 <112400160+Simay4955@users.noreply.github.com>
2022-10-21 09:59:38 +0300

    string   variables added

---
+++ dart/string.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  String s0 = " ";
+  String s1 = "Rukiye";
+  String s2 = 'Simay';
+  String s3 = 'Van\ da';
+  String s4 = " İstanbul'da ";
+  String s5 = ''' 
+  
+  1
+  2
+  3
+  4
+  5
+  ''';
+
+  String s6 = 'Rukiye' 'Simay' 'Asrrak';
+
+  print("$s0,$s1,$s2,$s3,$s4,$s5");
+}
