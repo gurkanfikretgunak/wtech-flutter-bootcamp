083ef56b53475539adf7d7fbd3b3c118aa7308a0
Elif Karagöz <75577514+eliffkaragoz@users.noreply.github.com>
2022-10-21 19:43:26 +0300

    veriables added

--- dart/strings.dart
+++ dart/strings.dart
@@ -1,1 +1,18 @@
-
+void main(List<String> args) {
+  String s0 = " ";
+  String s1 = "Elif";
+  String s2 = 'Karagöz';
+  String s3 = 'Antalya\'da';
+  String s4 = "İstanbul'da \n İzmir'de";
+  String s5 = ''' 
+  
+  1 
+  2
+  3
+  4
+  ''';
+  //String = List(Array Object)
+  String s6 = 'Dilan' 'Acur';
+  String s7 = r'Raw String';
+  print('$s0 $s1 $s2 $s3, $s4 $s5 $s6 $s7');
+}
---
+++ dart/veriables.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  var name = 'elif'; //string
+  var age = 26; //int
+  var weight = 48; // double
+  var hobby = [
+    "Movies",
+    "Running",
+  ]; //list (array)
+  var adress = {"adress_1": "Eskisehir", "adress_2": "Earth"};
+
+  print("$name,$age,$weight,$hobby,$adress");
+}
