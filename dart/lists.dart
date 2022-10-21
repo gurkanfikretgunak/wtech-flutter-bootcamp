c11e65ce15167e2e10776f83cfd1204e8d4fbd47
dilanacur1 <dilanacur1@gmail.com>
2022-10-21 19:00:23 +0300

    list added

---
+++ dart/lists.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  var l1 = [
+    1,
+    2,
+    3,
+    4,
+    "",
+    1.2,
+    [],
+    {"key": "value"},
+    {"value"}
+  ];
+  List l2 = <int>[1, 2, 3, 4, 5];
+  List l3 = <String>["İbrahim", "Samet", "Aslı", "Tuğba"];
+  List l4 = [1, "String", 1.2, []];
+
+  print('$l1 $l2 $l3 $l4');
+}
