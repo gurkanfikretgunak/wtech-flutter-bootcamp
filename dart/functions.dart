b73be8ca0ec7e1008319611750ab54729a3da4c7
Aysenur Perker <nurperker15@gmail.com>
2022-10-21 21:16:00 +0300

    functions added

---
+++ dart/functions.dart
@@ -0,0 +1,18 @@
+signInWithEmail() {
+//TODO sign-in with E-mail servies
+}
+
+String signInWithGithub() => "String function value";
+
+void getGithubUser() {}
+
+urlShorter(
+  String currentUrl, {
+  bool? withDot,
+}) {}
+
+signInWithEmailAndPassword({required String? email, String? password}) {}
+void main(List<String> args) {
+  signInWithEmailAndPassword(email: "aysenur@gmail", password: "123");
+  urlShorter("www", withDot: true);
+}
