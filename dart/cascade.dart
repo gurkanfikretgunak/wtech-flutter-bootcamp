a532a75b245d9001ba0420d02d37d85c70a0ccda
bhrzybk <103609875+bhrzybk@users.noreply.github.com>
2022-10-23 21:39:35 +0300

    cascade notation added

---
+++ dart/cascade.dart
@@ -0,0 +1,12 @@
+var paint = Paint()
+  ..color = Colors.black
+  ..strokeCap = StrokeCap.round
+  ..strokeWidth = 5.0;
+
+
+//üstteki örnek bu koda eşdeğerdir:
+
+var paint = Paint();
+paint.color = Colors.black;
+paint.strokeCap = StrokeCap.round;
+paint.strokeWidth = 5.0;
