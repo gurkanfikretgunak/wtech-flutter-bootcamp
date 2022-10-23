5bee8c3eea9034c0adace870f01af43795dce44b
Esra Ertsak <ertsakesra@gmail.com>
2022-10-23 18:43:28 +0300

    list add example added

---
+++ dart/list_add.dart
@@ -0,0 +1,7 @@
+void main() {
+  var list1 = [];
+  list1.add(11);
+  list1.add(15);
+  print(list1);
+  print(list1[1]);
+}
--- dart/strings.dart
+++ dart/strings.dart
@@ -5,4 +5,5 @@ void main(List<String> args) {
   String s2 = 'Ertsak';
   String s3 = 'Adana\'da';
   String s4 = "İstanbul'da";
+  print('$s0,$s1,$s2,$s3,$s4');
 }
