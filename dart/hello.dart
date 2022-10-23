4a3cf4f1cc4050c9a12b5f21edbf8b0b727c83d1
GayeSinan <59527099+GayeSinan@users.noreply.github.com>
2022-10-24 01:27:29 +0300

    hellocommit

--- dart/hello.dart
+++ dart/hello.dart
@@ -1,6 +1,12 @@
 main() {
   String hello = "hello flutter";
   print(hello);
-  // aynı işlem
   print('Hello flutter!');
+  int yil = 2022;
+  print(yil);
+  double oran = 2.22;
+  print(oran);
+  // toString kullanımı ise hello ve yılı yazdırırken uyuşmaınca kullanılır.
+  //CamelCase yani iki kelime olan yazılarda baş harfler büyük olmalı veya
+  //  _ çizgi ile ayrılmalı
 }
