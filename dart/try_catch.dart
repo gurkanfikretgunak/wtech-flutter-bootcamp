f53dacbbc7fcbe136a13215ee05ccc49043cd7d1
Beyza Aktürk <48009801+beyzakturk@users.noreply.github.com>
2022-10-24 20:23:59 +0300

    add try-catch

---
+++ dart/try_catch.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  // metods or func 'code'
+  void myFunction() {
+    currentUser(String email, String userID) {
+      try {
+        checkTheUser() {}
+      } on Exception catch (e) {
+        print(e);
+      } finally {
+        navigateToHome(int errorCode) {}
+      }
+    }
+  }
+}
