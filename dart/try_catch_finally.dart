f21f96b3f9fa07ec750acecf404cd5db9d5a4ac3
Melih Çelik <melihcelik0909@gmail.com>
2022-10-24 21:33:44 +0300

    try catch added

---
+++ dart/try_catch_finally.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  void currentUser(String mail, String userID) {
+    try {
+      checkUser() {}
+    } on CustomException catch (e) {
+      print(e);
+    } finally {
+      navigateToLogin(int errorCode) {}
+    }
+  }
+}
+
+class CustomException {}
