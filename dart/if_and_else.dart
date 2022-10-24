5b3fab3ea2ebb7618d38576b7777e51eabf658dc
Batuhan-Akdemirr <85792583+Batuhan-Akdemirr@users.noreply.github.com>
2022-10-24 12:42:39 +0300

    if and else added

---
+++ dart/if_and_else.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  int value1 = 10, value2 = 20;
+
+  if (value1 > value2)
+    print('value1 is greater than value2');
+  else if (value1 == value2)
+    print('value 1 is equal to value2');
+  else
+    print('value1 is less than value2');
+}
