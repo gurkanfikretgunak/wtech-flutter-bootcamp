73f1d3afd1ca22aa644e4a59784a94d9f8715bbb
Aslınaz Dayı <aslinazdayi@gmail.com>
2022-10-24 20:23:19 +0300

    try-catch examples had done

---
+++ dart/try_catch_finally.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+
+  //methods or functions "code snaps"
+
+void currentUser(String email,String userID){
+
+    try {
+      checkTheUser(){}
+    } on FirebaseException catch (e) {
+      print(e);
+    }finally {
+      navigateToHome(int errorCode){}
+    }
+}
+}
+
+
+
+
+class FirebaseException{
+
+
+}
