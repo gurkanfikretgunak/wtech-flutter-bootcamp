8963783844f1bdca9ef6a58d42dfd68127928d48
Batuhan-Akdemirr <85792583+Batuhan-Akdemirr@users.noreply.github.com>
2022-10-24 12:49:44 +0300

    while and do while added

---
+++ dart/while_and_do_while.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  int counter = 10;
+
+  while (counter > 0) {
+    print('Wtech');
+    counter--;
+  }
+  counter = 10;
+  print('******************************');
+  do {
+    counter--;
+    print('Wtech');
+  } while (counter > 0);
+}
