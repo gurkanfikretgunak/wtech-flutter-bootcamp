f51d3b7e4be156c74e5d7307fb3846fcf9b1b871
Ulaş Öztürk <ulasozturkdev@gmail.com>
2022-10-24 10:56:06 +0300

    ex

---
+++ dart/example.dart
@@ -0,0 +1,16 @@
+
+
+void main(List<String> args) {
+  
+
+
+  List<String> liste = ["ulas","ulassss","ozturk","ozturkkk"];
+
+  int i;
+  for(i=0;i<liste.length;i++){
+    print(liste[i].length);
+  }
+
+  
+
+}
