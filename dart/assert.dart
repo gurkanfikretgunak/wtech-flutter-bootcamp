90625726df7997611ff939def891b8ffe4d25eab
Elif Karagöz <75577514+eliffkaragoz@users.noreply.github.com>
2022-10-24 12:26:01 +0300

    assert added

---
+++ dart/assert.dart
@@ -0,0 +1,10 @@
+/*Hata kontrolleri için kullanılır.
+  Assert içerisindeki ifade true ise kod çalışmaya devam eder. False ise error verir.
+
+*/
+
+void main() {
+  String geek = "example";
+  assert(geek != "elif karagöz");
+  print("You Can See This Line Geek as a Output");
+}
