880e89f3abd6b542f44ddd728a285ad8d72e71c1
Batuhan-Akdemirr <85792583+Batuhan-Akdemirr@users.noreply.github.com>
2022-10-24 12:08:21 +0300

    Equality and relational operators added

---
+++ dart/equality_reletional_operators.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  int value1 = 10, value2 = 20;
+
+  if (value1 == value2) print('value1 is equal to value2 ');
+  if (value1 != value2) print('value1 isn\'t equal to value2');
+  if (value1 > value2) print('value1 greater than value2');
+  if (value1 < value2) print('value1 less than value2 ');
+  if (value1 >= value2) print('value1 greater than or equal to value2');
+  if (value1 <= value2) print('value1 less than or equal to value2');
+}
