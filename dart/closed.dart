12d140f7e579fd8c3775b56c594c4cd7474b533d
Muhammed18-09 <79153020+Muhammed18-09@users.noreply.github.com>
2022-10-24 18:43:18 +0300

    Create closed.dart

---
+++ dart/closed.dart
@@ -0,0 +1,14 @@
+Function makeAdder(int addBy) {
+  return (int i) => addBy + i;
+}
+
+void main() {
+  // Create a function that adds 2.
+  var add2 = makeAdder(2);
+
+  // Create a function that adds 4.
+  var add4 = makeAdder(4);
+
+  assert(add2(3) == 5);
+  assert(add4(3) == 7);
+}
