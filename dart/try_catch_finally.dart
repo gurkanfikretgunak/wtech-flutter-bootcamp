376ed11640d2d8652e131453944823e0b0d423c8
Gökçe Yılmaz <58955905+gokceyilmazz@users.noreply.github.com>
2022-10-24 20:23:28 +0300

    - try catch

---
+++ dart/try_catch_finally.dart
@@ -0,0 +1,14 @@
+void main(List<String> args){
+
+  void currentUser(String email, String userID){
+    try{
+      checkTheUser(){}
+      } on FirebaseException catch(e) {
+        print(e);
+      }finally{
+        navigateToHome(int errorCode){}
+      }
+    }
+  }
+
+class FirebaseException{}
