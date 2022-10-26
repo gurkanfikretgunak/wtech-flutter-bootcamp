ef402f3214425fdaa3bc1e8939f74ecadf01bb86
emineealtuntas <111460539+emineealtuntas@users.noreply.github.com>
2022-10-26 18:24:16 +0300

    setter getter added

---
+++ dart/setter_getter.dart
@@ -0,0 +1,36 @@
+//classlar içinde oluşturulan örnek değişkenlerin değerlerini kaydetmek veya o değerleri okumak için kullanılan yapılardır
+//getter: okuyucu
+//Setter: kaydedici
+
+void main(List<String> args) {
+  Student student1 = Student();
+  student1.name =
+      "Emine"; //bu setter sayesinde mümkün oldu. özel olarak oluşturmasakta default olarak oluşturulmuş setter getter yapıları var
+  print("Öğrenci adı: ${student1.name}"); //getter
+
+  Students student2 = Students();
+  student2.success = 5;
+  print("Öğrenci yüzdesi: ${student2.success}");
+}
+
+class Student {
+  var name;
+}
+
+class Students {
+  var name1;
+  var percent;
+
+  set success(int puan) {
+    var score;
+    if (score > 10)
+      var score = 10;
+    else if (score < 0) score = 0;
+
+    percent = score * 10;
+  }
+
+  int get success {
+    return percent;
+  }
+}
