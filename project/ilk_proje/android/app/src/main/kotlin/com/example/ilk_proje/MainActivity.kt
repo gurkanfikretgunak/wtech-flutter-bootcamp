85f74b8ff21c91b69f097a09f39d8d64bb1d8510
Ahmet Aydın <84200491+ahmtydn@users.noreply.github.com>
2022-10-26 21:20:33 +0300

    flutter first app init

---
+++ project/ilk_proje/android/app/src/main/kotlin/com/example/ilk_proje/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ilk_proje
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ project/ilk_proje/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Wtech Bootcamp"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Ahmet Aydın',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
