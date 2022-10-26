764ef53ef0d37846152c4434a62d5c4bafea96f0
dilanacur1 <dilanacur1@gmail.com>
2022-10-26 18:48:18 +0300

    class added

---
+++ dart/class.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {}
+
+class Ogrenci {
+  String? adi; 
+  String? soyAdi;
+  int? notu;
+  bool? gectimi;
+}
+// Değişken tanımlarken soru işareti 
+//Sebebi Dart Null Safety özelliği. Daha sonra değer verilir
+//şimdilik null ol demek istedik. Soru işareti koymasaydık
+//varsayılan olarak bir değer atamamız gerekirdi. 
+//alttaki örnekte varsayılan değerler atadık. 
+//alttaki örnekte içerisine bir fonksiyon ekledik
+
+
+
+  String adi = "";
+  String soyAdi = "";
+  int notu = 0;
+  bool gectimi = true;
+ 
