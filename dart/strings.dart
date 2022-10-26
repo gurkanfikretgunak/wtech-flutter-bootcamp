8a720c0eaa59d927d0a027e884da8624b565d44f
Sebnemeryilmaz <116290377+Sebnemeryilmaz@users.noreply.github.com>
2022-10-26 18:52:52 +0300

    string.dart added

--- dart/strings.dart
+++ dart/strings.dart
@@ -1,3 +1,22 @@
 void main(List<String> args) {
-  
+  string s0 = " ";
+  string s1 = "Sebnem";
+  string s2 = 'Eryılmaz';
+  string s3 = 'Antalya\'da';
+  string s4 = "İstanbul'da \ İzmir'de";
+  string s5 = '''
+
+1
+2
+3
+4
+ 
+ ''';
+
+ //string = List(Array Object)
+ string s6 = 'Sebnem' 'Eryılmaz';
+ string s7 = r'Raw String';
+ print('$s0 $s1 $s2 $s3 $s4 $s5 $s6 $s7');
+ 
+
 }
