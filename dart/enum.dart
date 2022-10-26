02fd34800d1d407a9f1b3e5acb77e9bb0b00795a
Elif Karagöz <75577514+eliffkaragoz@users.noreply.github.com>
2022-10-26 18:49:17 +0300

    enum added

---
+++ dart/enum.dart
@@ -0,0 +1,47 @@
+/** Sabit sayıda sabit değerleri temsil etmek için kullanılır.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+enum Color { red, green, blue }
+
+method() {
+  final favoriteColor = Color.blue;
+  if (Color.red.index == 2) {
+    print("dfdf");
+  }
+}
+
+enum Weather {
+  sunny,
+  cloudy,
+  rainy,
+}
+
+extension WeatherExt on Weather {
+  static const weatherMap = {
+    Weather.sunny: "What a lovely weather",
+    Weather.cloudy: "Scattered showers predicted",
+    Weather.rainy: "Will be raining today",
+  };
+
+  void console() {
+    print("${this.index} ${this.about}");
+  }
+
+  String get about => weatherMap[this]!;
+}
+
+enum ImageEnum { image }
+
+extension ImageExtension on ImageEnum {
+  Image toImage(String imgName,
+          {required double? width, required double? height}) =>
+      Image.asset("assets/images/${imgName}.png", height: height, width: width);
+}
+
+class Image {}
