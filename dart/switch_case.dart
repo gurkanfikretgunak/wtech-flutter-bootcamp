5e0c1b561d9405d2c74a45f50dead0f1877ead86
dilanacur1 <dilanacur1@gmail.com>
2022-10-27 19:01:30 +0300

    switch_case learned

---
+++ dart/switch_case.dart
@@ -0,0 +1,31 @@
+void main(){
+  
+  String not= "D";
+  switch(not){
+    case "A":{print("süper");}
+      break;
+    case "B": {print("iyi");}
+      break;
+    case "C": {print("orta");}
+      break;
+    case "D": {print("kötü");}
+      break;
+    default: {print("bilinmiyor");}
+      break;
+  }
+    int i = 52;
+  switch (i) {
+    case 0:
+      print("i'nin değeri 0'dır.");
+      break;
+    case 5:
+      print("i'nin değeri 5'tir.");
+      break;
+    case 10:
+      print("i'nin değeri 10'dur.");
+      break;
+    default:
+      print("i'nin değeri bilinmiyor.");
+  }
+  }    
+    
