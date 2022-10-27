21191db604546a6a12ec2986ac8a7d3561bf4dbf
Elif Karagöz <75577514+eliffkaragoz@users.noreply.github.com>
2022-10-27 22:23:54 +0300

    if-else added

---
+++ dart/if_else.dart
@@ -0,0 +1,15 @@
+main() {
+  if (7 % 2 == 0) {
+    print('7 is even');
+  } else {
+    print('7 is odd');
+  }
+
+  if (8 % 4 == 0) {
+    print('8 is divisble by 4');
+  }
+
+  bool isActive = false;
+  var buttonColor = isActive ? "pink" : "gray";
+  print(buttonColor);
+}
