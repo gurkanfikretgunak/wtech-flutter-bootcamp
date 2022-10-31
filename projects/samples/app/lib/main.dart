e421f0e338c03b87341e8e9a778c2278fb90935f
busraalpay <busalpay@gmail.com>
2022-10-31 18:47:58 +0300

    new

---
+++ projects/samples/app/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomeView(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: false,
+        primarySwatch: Colors.blueGrey,
+      ),
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      child: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Icon(Icons.home),
+                  Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "About ",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Details",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+        color: Colors.pink,
+      ),
+    );
+  }
+}
