cdf0c0fef997b0d72989ff2d048253aca0d9a0c1
Ulaş Öztürk <ulasozturkdev@gmail.com>
2022-10-31 21:35:16 +0300

    project added

--- flutter/wtech_bootcamp_learn/lib/core/app.dart
+++ flutter/wtech_bootcamp_learn/lib/core/app.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
 
-import 'views/home_view.dart';
+
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       
-      home: HomeView(),
+      home: MyApp(),
     );
   }
 }
---
+++ flutter/wtech_bootcamp_learn/lib/core/themes/custom_theme_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomTheme{
+
+    static ThemeData customThemeData() => ThemeData(
+      useMaterial3: false,
+      primarySwatch: Colors.pink,
+      
+    );
+}
