5a0ff42517e712d4c1d84e83ebb2e1b26ae59b60
Ulaş Öztürk <ulasozturkdev@gmail.com>
2022-11-02 17:12:29 +0300

    sample screen added

--- projects/samples/app/test/widget_test.dart
+++ projects/samples/app/test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:samples/main.dart';
+import 'package:samples/views/my_home_view.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyHomeView());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
