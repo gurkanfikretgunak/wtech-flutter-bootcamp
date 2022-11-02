099077d9a70208c7d9891bba2d5c0d92821bcd47
Esra Ertsak <ertsakesra@gmail.com>
2022-11-02 20:50:52 +0300

    icon_button added

--- projects/samples/app/lib/main.dart
+++ projects/samples/app/lib/main.dart
@@ -6,3 +6,17 @@ void main() {
   runApp(const App());
 }
 
+IconButton _buildPlayPauseButton() {
+  return IconButton(
+    icon:
+    (widget.isPlaying)
+        ? Icon(Icons.pause)
+        : Icon(Icons.play_arrow),
+    color: Colors.white,
+    onPressed: () {
+      if (widget.onPlayStateChanged != null) {
+        widget.onPlayStateChanged(!widget.isPlaying);
+      }
+    },
+  );
+}
