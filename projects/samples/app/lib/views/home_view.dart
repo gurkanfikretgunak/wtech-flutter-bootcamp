11bd807c4621f9df6cce9759852439625e4e1d7b
Aysenur Perker <nurperker15@gmail.com>
2022-11-02 19:56:14 +0300

    card widget added

--- projects/samples/app/lib/views/home_view.dart
+++ projects/samples/app/lib/views/home_view.dart
@@ -12,11 +12,52 @@ class HomeView extends StatelessWidget {
     return Scaffold(
         appBar: CustomWidgets.customAppBar(),
         body: const Center(
-          child: Text(
-            'Hello',
-            style: TextStyle(color: Colors.black),
+          child: SizedBox(
+            width: 300,
+            child: CardWidget(),
           ),
         ),
         bottomNavigationBar: CustomWidgets.customBottomNavBar());
+  }
+}
+
+class CardWidget extends StatelessWidget {
+  const CardWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      color: Colors.red,
+      elevation: 10,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const ListTile(
+            leading: Icon(Icons.album, size: 70),
+            title: Text('Ayşenur Perker', style: TextStyle(color: Colors.white)),
+            subtitle: Text('Developer', style: TextStyle(color: Colors.white)),
+          ),
+          ButtonTheme(
+            child: ButtonBar(
+              children: <Widget>[
+                TextButton(
+                  child: const Text('Edit', style: TextStyle(color: Colors.white)),
+                  onPressed: () {},
+                ),
+                TextButton(
+                  child: const Text('Delete', style: TextStyle(color: Colors.white)),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
