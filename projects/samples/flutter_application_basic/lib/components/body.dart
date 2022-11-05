0bd3612922afcca226b36326dc602e04bed362dc
emineealtuntas <111460539+emineealtuntas@users.noreply.github.com>
2022-11-05 19:48:13 +0300

    added changes to body

--- projects/samples/flutter_application_basic/lib/components/body.dart
+++ projects/samples/flutter_application_basic/lib/components/body.dart
@@ -2,42 +2,114 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_basic/constants.dart';
+import 'package:flutter_application_basic/screens/home/components/home_screen.dart';
 
 class Body extends StatelessWidget {
+  const Body({super.key});
+
   @override
   Widget build(BuildContext context) {
     // It will provide us total height and width of our screen
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: <Widget>[
-        // ignore: sized_box_for_whitespace
-        Container(
-          // It will cover 20% of our total height
-          height: size.height * 0.2,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                height: size.height * 0.2 - 27,
-                // ignore: prefer_const_constructors
-                decoration: BoxDecoration(
-                    color: kPrimaryColor,
-                    borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(36),
-                      bottomRight: Radius.circular(36),
-                    )),
+    // it enable scrolling on small device
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          // ignore: sized_box_for_whitespace
+          HeaderWithSearchBox(size: size)
+        ],
+      ),
+    );
+  }
+}
+
+class HeaderWithSearchBox extends StatelessWidget {
+  const HeaderWithSearchBox({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // It will cover 20% of our total height
+      height: size.height * 0.2,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+              bottom: 36 + kDefaultPadding,
+            ),
+            height: size.height * 0.2 - 27,
+            // ignore: prefer_const_constructors
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(36),
+                bottomRight: Radius.circular(36),
               ),
-              Positioned(
-                child: Container(
-                  height: 54,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                  ),
+            ),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  'Merhaba!',
+                  style: Theme.of(context).textTheme.headline5.copyWith(
+                      color: Colors.white, fontWeight: FontWeight.bold),
                 ),
-              )
-            ],
+                Spacer(),
+                Image.asset("assets/images/logo.png")
+              ],
+            ),
           ),
-        )
-      ],
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              height: 54,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(0.23),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {},
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        hintStyle: TextStyle(
+                          color: kPrimaryColor.withOpacity(0.5),
+                        ),
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        // surflix isn't working properly with SVG
+                        // thats why we use row
+                        // suffixIcon: SvgPicture.asset("assets/icons/search.svg"),
+                      ),
+                    ),
+                  ),
+                  SvgPicture.asset("assets/icons/search.svg"),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
