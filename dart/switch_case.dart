6c9f44aaa5e71be42bb72e62c1144d2c23e0501f
busraalpay <busalpay@gmail.com>
2022-11-06 01:19:40 +0300

    switch_case dart example

---
+++ dart/switch_case.dart
@@ -0,0 +1,18 @@
+main() {
+  var color = 'purple';
+  switch (color) {
+    case 'red':
+      print('red');
+      break;
+    case 'yellow':
+      print('yellow');
+      break;
+    case 'green':
+      print('green');
+      break;
+    case 'purple':
+      print('purple');
+      break;
+    default:
+  }
+}
