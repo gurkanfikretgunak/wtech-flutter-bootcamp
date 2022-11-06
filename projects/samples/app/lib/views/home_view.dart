2c1282085510c74144a18eb1b1f6e8adadd41827
Ecem Hacıoğlu <74303639+ecemhacioglu@users.noreply.github.com>
2022-11-07 01:45:55 +0300

    custom widget design

--- projects/samples/app/lib/views/home_view.dart
+++ projects/samples/app/lib/views/home_view.dart
@@ -9,14 +9,31 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool? customAppBar= true;
       return Scaffold(
-        extendBody: true,
+        appBar: customAppBar? CustomWidgets.customAppBar()
+        :AppBar(
+          leading: const Icon(Icons.menu),
+           title: const Text('Page title'),
+           actions: const [
+            Icon(Icons.favorite),
+            Padding(
+               padding: EdgeInsets.symmetric(horizontal: 16),
+               child: Icon(Icons.search),
+              ),
+           Icon(Icons.more_vert),
+  ],
+        ),
+      extendBody: true,
       floatingActionButton: CustomWidgets.getFloatingActionButton(),
-floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
 
       body: Container(
-        color: Colors.grey),
-      bottomNavigationBar: CustomWidgets.getBottomAppbarWidget(),
+        color: Colors.grey,
+        child: Column(
+          children:[CustomWidgets.customAppBanner()] 
+           )),
+      bottomNavigationBar: CustomWidgets.customBottomNavBar(),
 
 
     );
--- projects/samples/app/lib/widgets/custom_widget.dart
+++ projects/samples/app/lib/widgets/custom_widget.dart
@@ -3,6 +3,108 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CustomWidgets{
+ static Widget customBottomNavBar()
+{
+  return BottomNavigationBar(
+  type: BottomNavigationBarType.fixed,
+  backgroundColor: Colors.pink,
+  selectedItemColor: Colors.white,
+  unselectedItemColor: Colors.white.withOpacity(.60),
+  selectedFontSize: 15,
+  unselectedFontSize: 10,
+  onTap: (value) {
+    // Respond to item press.
+  },
+  // ignore: prefer_const_literals_to_create_immutables
+  items: [
+    // ignore: prefer_const_constructors
+    BottomNavigationBarItem(
+      label: "FAV",
+      icon: const Icon(Icons.favorite),
+    ),
+    // ignore: prefer_const_constructors
+    BottomNavigationBarItem(
+      label: "MUSIC",
+      icon: const Icon(Icons.music_note),
+    ),
+    // ignore: prefer_const_constructors
+    BottomNavigationBarItem(
+      label: "LOCATION",
+      icon: const Icon(Icons.location_on),
+    ),
+    // ignore: prefer_const_constructors
+    BottomNavigationBarItem(
+      label: "LIBRARY",
+      icon: const Icon(Icons.library_books),
+    ),
+  ],
+);
+
+
+} 
+
+
+static Widget customAppBanner() {
+
+  return Padding( 
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      children: [
+        Text("Material Banner Widget"),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: MaterialBanner(
+            content: const Text('Error message text'),
+            leading: const CircleAvatar(child: Icon(Icons.delete)),
+            actions: [
+          TextButton(
+            child: const Text('ACTION 1'),
+            onPressed: () { },
+          ),
+              TextButton(
+            child: const Text('ACTION 2'),
+            onPressed: () { },
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+} 
+
+
+  static PreferredSize customAppBar(){
+    return PreferredSize(
+          // ignore: sort_child_properties_last
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // ignore: prefer_const_constructors
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Text("Wtech App"),
+                    ),
+                  
+                  IconButton(
+                    onPressed:(){
+                      if (kDebugMode) {
+                        print("Icon Right Button");
+                      }
+                    },
+                    icon: const Icon(Icons.face))
+                ],
+              ),
+            ],
+            ),
+          
+          preferredSize: const Size.fromHeight(30),
+        );
+
+  } 
 
 static Widget getFloatingActionButton(){
   return FloatingActionButton(
