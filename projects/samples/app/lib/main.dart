6ce0fd6ded809bd353e1d87d8f9da2d918758dd2
busraalpay <busalpay@gmail.com>
2022-11-07 16:55:18 +0300

    MaterialApp ve Scaffold Widgets

---
+++ projects/samples/app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Baslik'),
+          backgroundColor: Colors.pink
+         
+        ),
+        body: Text('Wtech Bootcamp'),),
+    );
+  }
+}
