75baa3429e7bb7e9d0f4fce6f314d2a27f58d3ea
busraalpay <busalpay@gmail.com>
2022-11-07 17:23:57 +0300

    FloatingActionButton, ıcons, colors, font

--- projects/samples/app/lib/main.dart
+++ projects/samples/app/lib/main.dart
@@ -9,13 +9,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.teal, accentColor: Colors.purpleAccent
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Baslik'),
-          backgroundColor: Colors.pink
-         
+          title: Text('Baslik'), 
         ),
-        body: Text('Wtech Bootcamp'),),
+        body: Text('Wtech Bootcamp', 
+        style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.w200),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            debugPrint('Tıklandı');
+          },
+          child: Icon(
+            Icons.headphones,
+            color:Colors.black,
+            ),
+          backgroundColor: Colors.pink,
+        ),
+      ),
     );
   }
 }
