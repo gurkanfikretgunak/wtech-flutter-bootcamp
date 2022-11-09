19198c121c5e3e780b1aadc1cdd8b313e5ea6848
emineealtuntas <111460539+emineealtuntas@users.noreply.github.com>
2022-11-09 17:54:41 +0300

    buttons edited

--- projects/samples/flutter_application_basic/lib/components/body.dart
+++ projects/samples/flutter_application_basic/lib/components/body.dart
@@ -5,6 +5,7 @@ import 'package:flutter_application_basic/constants.dart';
 import 'package:flutter_application_basic/screens/home/home_screen.dart';
 
 import 'header_with_searchbox.dart';
+import 'title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -19,53 +20,34 @@ class Body extends StatelessWidget {
         children: <Widget>[
           // ignore: sized_box_for_whitespace
           HeaderWithSearchBox(size: size),
-          Row(
-            children: [
-              const TitleWithCustomUnderline(text: "Öneriler"),
-              TextButton(
-                style: TextButton.styleFrom(foregroundColor: Colors.green),
-                onPressed: () {},
-                child: const Text("Fazlası"),
-              )
-            ],
+          TitleWithMoreBtn(
+            title: "Öneriler",
+            press: () {},
+          ),
+          // it will coner 40% of our total width
+          Container(
+            width: size.width * 0.4,
+            child: Column(
+              children: <Widget>[
+                Image.asset("assets/images/image_1.png"),
+                Container(
+                  padding: EdgeInsets.all(kDefaultPadding / 2),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.23),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           )
         ],
       ),
-    );
-  }
-}
-
-class TitleWithCustomUnderline extends StatelessWidget {
-  const TitleWithCustomUnderline({
-    Key? key,
-    required this.text,
-  }) : super(key: key);
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 24,
-      child: Stack(children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(left: kDefaultPadding / 4),
-          child: Text(
-            text,
-            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Positioned(
-          bottom: 0,
-          left: 0,
-          right: 0,
-          child: Container(
-            margin: const EdgeInsets.only(right: kDefaultPadding / 4),
-            height: 7,
-            color: kPrimaryColor.withOpacity(0.2),
-          ),
-        )
-      ]),
     );
   }
 }
