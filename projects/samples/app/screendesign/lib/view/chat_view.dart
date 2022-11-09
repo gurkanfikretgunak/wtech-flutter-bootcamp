8f9cc3be883d5424f4f87bebed6bdc35493f6891
Muhammed18-09 <79153020+Muhammed18-09@users.noreply.github.com>
2022-11-09 16:49:53 +0300

    chat Message

--- projects/samples/app/screendesign/lib/view/chat_view.dart
+++ projects/samples/app/screendesign/lib/view/chat_view.dart
@@ -5,14 +5,60 @@ class Chatscreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.amber,
-      body: Column(children: []),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.green,
-        child: Icon(Icons.message),
-      ),
-    );
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.green,
+            child: Icon(Icons.message),
+          ),
+          appBar: AppBar(
+            actions: [
+              InkWell(child: const Icon(Icons.search), onTap: () {}),
+              PopupMenuButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                itemBuilder: (context) {
+                  return [
+                    // In this case, we need 5 popupmenuItems one for each option.
+                    const PopupMenuItem(child: Text('New Group')),
+                    const PopupMenuItem(child: Text('New Broadcast')),
+                    const PopupMenuItem(child: Text('Linked Devices')),
+                    const PopupMenuItem(child: Text('Starred Messages')),
+                    const PopupMenuItem(child: Text('Settings')),
+                  ];
+                },
+              ),
+            ],
+            backgroundColor: const Color(0xff128C7E),
+            title: const Text('WhatsApp'),
+            bottom: const TabBar(
+              indicatorSize: TabBarIndicatorSize.tab,
+              indicatorColor: Colors.white,
+              tabs: [
+                Tab(
+                    iconMargin: EdgeInsets.all(100),
+                    child: Icon(
+                      Icons.camera_alt_rounded,
+                    )),
+                Tab(
+                  child: Text('CHATS', style: TextStyle(color: Colors.white)),
+                ),
+                Tab(
+                  child: Text('STATUS', style: TextStyle(color: Colors.white)),
+                ),
+                Tab(
+                  child: Text('CALLS', style: TextStyle(color: Colors.white)),
+                ),
+              ],
+              labelColor: Colors.white,
+            ),
+          ),
+          body: Column(
+            children: [],
+          ),
+        ));
   }
 }
