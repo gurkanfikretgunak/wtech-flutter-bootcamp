8d0815b0252b5d6f8fbbb2d57a4335b6da1c3ff2
Elif Karagöz <75577514+eliffkaragoz@users.noreply.github.com>
2022-11-10 02:10:16 +0300

    created  folder structures in coursera

---
+++ projects/coursera/lib/views/authentication/sign_in_view.dart
@@ -0,0 +1,11 @@
+import 'package:coursera/widgets/custom_scaffold.dart';
+import 'package:flutter/material.dart';
+
+class SignInView extends StatelessWidget {
+  const SignInView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(body: Column());
+  }
+}
