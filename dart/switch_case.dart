828fd486157fa04f3750e1e695f79b70979956ac
Esra Ertsak <ertsakesra@gmail.com>
2022-11-10 15:53:35 +0300

    drafts completed

---
+++ dart/switch_case.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  int i = 5;
+  switch (i) {
+    case 0:
+      print("i'nin değeri 0'dir.");
+      break;
+    case 5:
+      print("i'nin değeri 5'tir.");
+      break;
+    case 10:
+      print("i'nin değeri 10'dur.");
+      break;
+    default:
+      print("i'nin değeri bilinmiyor.");
+      break;
+  }
+}
