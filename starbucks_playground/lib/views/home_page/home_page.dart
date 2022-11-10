5ce602fdb9334f15efc36fd0a303ae612daebd08
Simay4955 <112400160+Simay4955@users.noreply.github.com>
2022-11-11 00:31:38 +0300

    Splash Screen Update

--- starbucks_playground/lib/views/home_page/home_page.dart
+++ starbucks_playground/lib/views/home_page/home_page.dart
@@ -11,8 +11,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Color.fromARGB(255, 198, 181, 231),
       // ignore: prefer_const_constructors
-      appBar: AppBar(actions: [Icon(Icons.abc_outlined)]),
+    
       body: Center(child: Container(child: const Text("Home Page Welcome "))),
     );
   }
--- starbucks_playground/lib/widgets/splash/custom_splash_screen.dart
+++ starbucks_playground/lib/widgets/splash/custom_splash_screen.dart
@@ -1,23 +1,22 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-
+import 'package:page_transition/page_transition.dart';
 import 'package:flutter/material.dart';
 import 'package:starbucks_playground/views/home_page/home_page.dart';
-import 'package:page_transition/page_transition.dart';
+
 
 class CustomSplashScreen {
   static Widget customSplashScreen() {
     return AnimatedSplashScreen(
-      // icondaki taşmayı engeller 
+      // icondaki taşmayı engeller
       splashIconSize: 250,
       duration: 4000,
-      // İcona  animasyonlu gelişi 
+      // İcona  animasyonlu gelişi
       splashTransition: SplashTransition.scaleTransition,
-      backgroundColor:Colors.green.shade300,
-      pageTransitionType: PageTransitionType.rightToLeftWithFade,
-      animationDuration: Duration(seconds: 2),
+      backgroundColor: Colors.green.shade300,
+      pageTransitionType: PageTransitionType.bottomToTop,
+      animationDuration: const Duration(seconds: 2),
 
       splash: Column(
-       
         children: [
           ClipOval(
             child: Image.asset(
@@ -25,7 +24,6 @@ class CustomSplashScreen {
               height: 130,
             ),
           ),
-        
         ],
       ),
       nextScreen: const HomePage(),
