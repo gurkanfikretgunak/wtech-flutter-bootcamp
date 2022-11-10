8e577a16291178355008c57ac50225bfffcd3301
Esra Ertsak <ertsakesra@gmail.com>
2022-11-10 14:53:30 +0300

    draft completition commits

---
+++ dart/enum.dart
@@ -0,0 +1,25 @@
+/// Enum that enumerates the different planets in our solar system
+/// and some of their properties.
+
+enum PlanetType { terrestrial, gas, ice }
+
+enum Planet {
+  mercury(planetType: PlanetType.terrestrial, moons: 0, hasRings: false),
+  venus(planetType: PlanetType.terrestrial, moons: 0, hasRings: false),
+  // ···
+  uranus(planetType: PlanetType.ice, moons: 27, hasRings: true),
+  neptune(planetType: PlanetType.ice, moons: 14, hasRings: true);
+
+  /// A constant generating constructor
+  const Planet(
+      {required this.planetType, required this.moons, required this.hasRings});
+
+  /// All instance variables are final
+  final PlanetType planetType;
+  final int moons;
+  final bool hasRings;
+
+  /// Enhanced enums support getters and other methods
+  bool get isGiant =>
+      planetType == PlanetType.gas || planetType == PlanetType.ice;
+}
---
+++ dart/helloworld.dart
@@ -0,0 +1,5 @@
+//Every app has a main() function. To display text on the console, you can use the top-level print() function:
+void main() {
+  print("Hello World");
+  print("Esra Ertsak");
+}
