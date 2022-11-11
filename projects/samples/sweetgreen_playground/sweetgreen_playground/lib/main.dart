9282d198a2fb1d08bb77b1fe7cf5dc70e8963cae
Esra Ertsak <ertsakesra@gmail.com>
2022-11-11 13:26:29 +0300

    app logo added

--- projects/samples/sweetgreen_playground/sweetgreen_playground/lib/main.dart
+++ projects/samples/sweetgreen_playground/sweetgreen_playground/lib/main.dart
@@ -1,26 +1,21 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main()
- {
-  runApp
-  (
-   MyWidget()
-  );
-
+void main() {
+  runApp(MyWidget());
 }
+
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.green[900],
-      child: Center
-      (child:Text("sweetgreen",textDirection: TextDirection.ltr,) 
-      
-      ),
-      
+      color: const Color(0xFF1B5E20),
+      child: (
+        Image.asset(
+          "assets/resimler/sweetgreen-rebrands-removebg-preview.png")),
     );
   }
 }
-
