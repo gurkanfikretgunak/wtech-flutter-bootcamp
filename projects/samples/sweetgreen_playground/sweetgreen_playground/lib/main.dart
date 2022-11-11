36415b0f6c3c6f9b6e62425e0ed8463486e149ca
Esra Ertsak <ertsakesra@gmail.com>
2022-11-11 14:00:19 +0300

    main.dart packages imported

--- projects/samples/sweetgreen_playground/sweetgreen_playground/lib/main.dart
+++ projects/samples/sweetgreen_playground/sweetgreen_playground/lib/main.dart
@@ -1,20 +1,7 @@
-import 'dart:ui';
-
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:sweetgreen_playground/core/app.dart';
+import 'package:sweetgreen_playground/views/splash_view.dart';
 
 void main() {
-  runApp(MyWidget());
-}
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xFF1B5E20),
-      child: (Image.asset(
-          "assets/resimler/sweetgreen-rebrands-removebg-preview.png")),
-    );
-  }
+  runApp(const MyWidget());
 }
---
+++ projects/samples/sweetgreen_playground/sweetgreen_playground/lib/views/splash_view.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFF1B5E20),
+      child: (Image.asset(
+          "assets/resimler/sweetgreen-rebrands-removebg-preview.png")),
+    );
+  }
+}
