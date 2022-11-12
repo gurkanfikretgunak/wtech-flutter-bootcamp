836c426f2e82d404265f9eb6d10fb23b3581f277
Aysenur Perker <nurperker15@gmail.com>
2022-11-13 01:49:54 +0300

    retrofit classes created.

--- projects/todoist_playground/todoist_app/lib/service/data_service.dart
+++ projects/todoist_playground/todoist_app/lib/service/data_service.dart
@@ -1,12 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 import '../model/users_model.dart';
+
 part 'data_service.g.dart';
 
-@RestApi(baseUrl: "https://636eb123bb9cf402c807e3fd.mockapi.io")
-abstract class DataService {
-  factory DataService(Dio dio, {String baseUrl}) = _DataService;
+@RestApi(baseUrl: "https://636eb123bb9cf402c807e3fd.mockapi.io/")
+abstract class RestClient {
+  factory RestClient(Dio dio, {String baseUrl}) = _RestClient;
 
   @GET("/users")
   Future<List<Users>> getUsers();
+  @POST("/users")
+  Future<void> updateTasks(List<Users> users);
 }
---
+++ projects/todoist_playground/todoist_app/lib/service/data_service.g.dart
@@ -0,0 +1,81 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'data_service.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+
+class _RestClient implements RestClient {
+  _RestClient(
+    this._dio, {
+    this.baseUrl,
+  }) {
+    baseUrl ??= 'https://636eb123bb9cf402c807e3fd.mockapi.io/';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<List<Users>> getUsers() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result =
+        await _dio.fetch<List<dynamic>>(_setStreamType<List<Users>>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/users',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    var value = _result.data!
+        .map((dynamic i) => Users.fromJson(i as Map<String, dynamic>))
+        .toList();
+    return value;
+  }
+
+  @override
+  Future<void> updateTasks(users) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    await _dio.fetch<void>(_setStreamType<void>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/users',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    return null;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}
