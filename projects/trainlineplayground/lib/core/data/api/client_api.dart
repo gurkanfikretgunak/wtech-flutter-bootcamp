04ba097c96af15f4a1d1cdead66e201e2f46db3d
Ulaş Öztürk <ulasozturkdev@gmail.com>
2022-11-13 14:37:34 +0300

    service added

--- projects/trainlineplayground/lib/core/data/api/client_api.dart
+++ projects/trainlineplayground/lib/core/data/api/client_api.dart
@@ -1,6 +1,8 @@
-import 'package:json_annotation/json_annotation.dart';
+
 import 'package:retrofit/retrofit.dart';
 import 'package:dio/dio.dart';
+
+import '../models/tickets.dart';
 
 
 part 'client_api.g.dart';
@@ -13,22 +15,3 @@ abstract class RestClient {
   Future<List<Tickets>> getTasks();
 }
 
-@JsonSerializable()
-class Tickets{
-
-  String? trainTime;
-  String? trainarriveTime;
-  String? standardPrice;
-  String? firstClassPrice;
-  String? busTime;
-  String? busArriveTime;
-  String? busPrice;
-  String? kalkisYeri;
-  String? varisYeri;
-  String? id;
-
-  Tickets({this.trainTime,this.trainarriveTime,this.standardPrice,this.firstClassPrice,this.busTime,this.busArriveTime,this.busPrice,this.kalkisYeri,this.varisYeri,this.id});
-
-  factory Tickets.fromJson(Map<String, dynamic> json) => _$TicketsFromJson(json);
-  Map<String, dynamic> toJson() => _$TicketsToJson(this);
-}
