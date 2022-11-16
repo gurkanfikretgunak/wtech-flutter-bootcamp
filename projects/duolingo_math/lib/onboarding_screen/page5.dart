1d71bfc1bcd1bb652bb3b9e1984984be79cd9804
İbrahim Aydan <104987335+ibrahimaydan@users.noreply.github.com>
2022-11-16 15:09:35 +0300

    onboarding pages upgraded

--- projects/duolingo_math/lib/onboarding_screen/page5.dart
+++ projects/duolingo_math/lib/onboarding_screen/page5.dart
@@ -5,23 +5,36 @@ class Page5 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16, 240, 16, 0),
-            child: Text(
-              "How old are you ?",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
+    return Scaffold(
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16, 150, 16, 0),
+          child: Text(
+            "How old are you ?",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
             ),
           ),
-        ],
-      ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16, 50, 16, 0),
+          child: TextField(
+            keyboardType: TextInputType.number,
+            autofocus: true,
+            decoration: InputDecoration(
+                hintText: "Age in years",
+                hintStyle: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                border: OutlineInputBorder(
+                    borderRadius:
+                        const BorderRadius.all(Radius.circular(8.0)))),
+          ),
+        )
+      ]),
     );
   }
 }
