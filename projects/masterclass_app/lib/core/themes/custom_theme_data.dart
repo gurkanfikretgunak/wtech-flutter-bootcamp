b00cc70fcfd50e2ae2fe9535610d5f221d3ca527
cantanrikulu <73619391+cantanrikulu@users.noreply.github.com>
2022-11-18 20:01:05 +0300

    cutom theme added

---
+++ projects/masterclass_app/lib/core/themes/custom_theme_data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CustomTheme {
+  static ThemeData customThemeData() => ThemeData(
+        scaffoldBackgroundColor: Colors.black,
+        backgroundColor: Colors.black,
+      );
+}
