9c4e06908790a8599b0898236d3fd174c919389f
Ulaş Öztürk <ulasozturkdev@gmail.com>
2022-11-29 17:06:58 +0300

    some fixes added- to be continued

--- projects/trainlineplayground/lib/views/account_page.dart
+++ projects/trainlineplayground/lib/views/account_page.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:trainlineplayground/constants/constants.dart';
 
 import '../widgets/account_page_widgets.dart';
-
-
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
@@ -14,99 +13,46 @@ class AccountPage extends StatefulWidget {
 }
 
 class AccountPageState extends State<AccountPage> {
+  List<Widget> RowList = const [
+    MessagesRow(),
+    BookingRow(),
+    PaymentRow(),
+    RailCardsRow(),
+    BusinessRow(),
+    BusinesBookRow(),
+    MoneyRow(),
+    AppSettingsRow(),
+    PrivacyChoiceRow(),
+    PushRow(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-        const accountContainer(),
-        Container(
-          height: 70,
-          decoration:  BoxDecoration(
-            border: _bottomBorderSideWidget()
-          ),
-          child: const MessagesRow(),
-        ),
-        const BookingRow(),
-        
-        Container(
-          decoration: BoxDecoration(
-            border: _bottomBorderSideWidget()
-          ),
-          height: 50,
-          width: double.infinity,
-          child: const PaymentRow(),
-        ),
-        Container(
-          height: 50,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            border: _bottomBorderSideWidget()
-          ),
-          child: const RailCardsRow(),
-        ),
-        const BusinessRow(),
-        Container(
-          height: 50,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            border: _bottomBorderSideWidget()
-          ),
-          child: const BusinesBookRow(),
-        ),
-        const AppSettingsRow(),
-        Container(
-          height: 50,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            border: _bottomBorderSideWidget()
-          ),
-          child: const MoneyRow(),
-        ),
-        Container(
-           height: 50,
-           width: double.infinity,
-           decoration: BoxDecoration(
-            border: _bottomBorderSideWidget()
-          ),
-           child: const PrivacyChoiceRow(),
-        ),
-        Container(
-           height: 50,
-           width: double.infinity,
-           child: const PushRow(),
-        ),
-
-      ]),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-            icon: IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-            label: 'Search'),
-        BottomNavigationBarItem(
-            icon: IconButton(
-                onPressed: () {
-                  
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children:  [
+            const accountContainer(),
+            Container(
+              height: 500,
+              width: double.infinity,
+              child: ListView.builder(
+                itemCount: RowList.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 70,
+                    decoration: BoxDecoration(
+                      border: FunctionalAccounWidget.bottomBorderSideWidget()
+                    ),
+                    child: RowList[index],
+                  );
                 },
-                icon: const Icon(Icons.airplane_ticket_outlined)),
-            label: 'My Tickets'),
-        BottomNavigationBarItem(
-            icon: IconButton(onPressed: () {
-              Navigator.of(context).pushNamed('/');  
-              
-            },
-             icon: const Icon(Icons.person)),
-            label: 'Sign In'),
-      ]),
+                ),
+            )
+          ]),
+      bottomNavigationBar: AccountPageBottomNavBar(),
     );
-    
   }
-
-  Border _bottomBorderSideWidget() {
-    return  Border(bottom: BorderSide(
-            color: Colors.blue.shade100,
-            width: 1.0
-          ));
-  }
+  
 }
+
 
