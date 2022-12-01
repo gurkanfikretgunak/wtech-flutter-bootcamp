94207b81e8d7cbc5a835964c5894ac3df2ff432e
Aysenur Perker <nurperker15@gmail.com>
2022-12-02 01:45:13 +0300

    user todos added.

--- projects/todoist_playground/todoist_app/lib/views/home/todo_list_view.dart
+++ projects/todoist_playground/todoist_app/lib/views/home/todo_list_view.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
-import 'package:todoist_app/views/auth/pick_theme_page_view.dart';
-import 'package:todoist_app/views/settings/settings_app.dart';
-import '../../constants/text/home_page_contants.dart';
+import '../../constants/image/image_path_constants.dart';
 import '../../core/provider/service_provider.dart';
-import '../../widgets/custom_methods.dart';
+import '../../model/todos_model.dart';
 
 class TodoListView extends StatefulWidget {
   const TodoListView({Key? key}) : super(key: key);
@@ -16,50 +15,68 @@ class TodoListView extends StatefulWidget {
 class _TodoListViewState extends State<TodoListView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(CustomHomeText.todayText),
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search_rounded)),
-          IconButton(
-              onPressed: () {
-                CustomMethods.settingModalBottomSheet(context, const SettingsApp());
-              },
-              icon: const Icon(Icons.settings)),
-        ],
-      ),
-      body: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 200,
-            child: Consumer<ServiceProvider>(
-              builder: (context, value, child) {
-                return ListView.builder(
-                  itemCount: value.todosUser!.length,
-                  itemBuilder: (context, index) {
-                    return Column(
-                      children: [
-                        ListTile(
-                          title: Text(
-                            value.todosUser![index],
-                            style: TextStyle(color: Colors.black),
-                          ),
+    final size = MediaQuery.of(context).size;
+
+    return Consumer<ServiceProvider>(
+      builder: (context, value, child) {
+        return FutureBuilder<Todos?>(
+          future: value.fetchTodos(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "${snapshot.data!.date}",
+                      style: Theme.of(context).textTheme.headline1?.copyWith(fontSize: 20),
+                    ),
+                    const Divider(),
+                    SizedBox(
+                      height: 80,
+                      width: size.width,
+                      child: Card(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Icon(Icons.circle_outlined, color: Colors.red),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text("${snapshot.data!.title}",
+                                      style: Theme.of(context).textTheme.headline1?.copyWith(fontSize: 15)),
+                                  Text("${snapshot.data!.body}",
+                                      style: Theme.of(context).textTheme.caption?.copyWith(fontSize: 10)),
+                                  Text("${snapshot.data!.time} PM",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .caption
+                                          ?.copyWith(color: Colors.green, fontSize: 15)),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    );
-                  },
-                );
-              },
-            ),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          CustomMethods.settingModalBottomSheet(context, const ThemeSwitcher());
-        },
-        child: const Icon(Icons.add),
-      ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            } else {
+              return Center(
+                child: SizedBox(
+                  height: 30,
+                  width: 30,
+                  child: Lottie.asset(CustomImagePathConstants.loadingLottiePath),
+                ),
+              );
+            }
+          },
+        );
+      },
     );
   }
 }
