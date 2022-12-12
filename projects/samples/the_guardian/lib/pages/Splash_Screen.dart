a6760e748aaaefe48882b76dff69b9064899fd0a
Beyza Aktürk <48009801+beyzakturk@users.noreply.github.com>
2022-12-12 22:31:39 +0300

    add appbar widgets

--- projects/samples/the_guardian/lib/pages/Splash_Screen.dart
+++ projects/samples/the_guardian/lib/pages/Splash_Screen.dart
@@ -1,33 +1,19 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:the_guardian/pages/home_page.dart';
 
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration(seconds: 3), () {
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => HomePage()));
-    });
-  }
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        alignment: Alignment.bottomCenter,
-        decoration: BoxDecoration(
-          color: Color.fromARGB(255, 4, 35, 61),
-          image: DecorationImage(
-            image: AssetImage("assets/images/logo.png"),
-          ),
-        ),
-      ),
+    return MaterialApp(
+      home: AnimatedSplashScreen(
+          splash: Image.asset('assets/images/logo.png'),
+          duration: 3000,
+          splashTransition: SplashTransition.fadeTransition,
+          backgroundColor: Color(0xFF1A237E),
+          nextScreen: HomePage()),
     );
   }
 }
--- projects/samples/the_guardian/lib/pages/home_page.dart
+++ projects/samples/the_guardian/lib/pages/home_page.dart
@@ -1,77 +1,16 @@
-import 'package:flutter/material.dart';
-import 'package:the_guardian/pages/discover_page..dart';
-import 'package:the_guardian/pages/live_page.dart';
-import 'package:the_guardian/pages/menu_page.dart';
-import 'package:the_guardian/pages/saved_page.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
-  _HomePageState createState() => _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedPageIndex = 0;
-  List<Widget> _page = [
-    HomePage(),
-    LivePage(),
-    DiscoverPage(),
-    SavedPage(),
-    MenuPage(),
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Takip Sistemi'),
-      ),
-      body: _buildBody(),
-      bottomNavigationBar: _buildBottomNavigationBar(),
-    );
-  }
-
-  // Widget _buildAppBar() {
-  //   return AppBar(
-  //     title: const Text("Home Page"),
-  //   );
-  // } bu şekilde yazınca hata veriyor.
-
-  Widget _buildBody() {
-    return _page[_selectedPageIndex];
-  }
-
-  Widget _buildBottomNavigationBar() {
-    return BottomNavigationBar(
-      items: [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.schedule),
-          label: 'Live',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.travel_explore),
-          label: 'Discover',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.save),
-          label: 'Saved',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.menu),
-          label: 'Menu',
-        ),
-      ],
-      currentIndex: _selectedPageIndex, //New
-      onTap: _onTapItem,
-    );
-  }
-
-  void _onTapItem(int index) {
-    setState(() {
-      _selectedPageIndex = index;
-    });
+    return Container();
   }
 }
