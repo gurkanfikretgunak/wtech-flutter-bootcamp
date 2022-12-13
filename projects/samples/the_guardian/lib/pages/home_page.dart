23f1c2b38a58df5c1e6e0dda2b6f679c1912d189
Beyza Aktürk <48009801+beyzakturk@users.noreply.github.com>
2022-12-13 15:16:12 +0300

    add appbar

--- projects/samples/the_guardian/lib/pages/home_page.dart
+++ projects/samples/the_guardian/lib/pages/home_page.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +10,36 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Upgrade to Premium',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.yellow),
+          ),
+          backgroundColor: Color(0xFF1A237E),
+          leading: Builder(
+            builder: (BuildContext context) {
+              return ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'Support us',
+                  style: TextStyle(color: Colors.yellow),
+                ),
+              );
+            },
+          ),
+          actions: [
+            Text(
+              'The Guardian',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
